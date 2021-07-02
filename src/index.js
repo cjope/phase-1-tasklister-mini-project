46bd3c01868749f27aa1338078a320f7ddf22a47
Craig <craigjope@gmail.com>
2021-07-02 15:19:45 -0600

    I guess this is completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.getElementById("create-task-form").addEventListener("submit", function(event){
+    document.getElementById("create-task-form").value += document.getElementById("create-task-form".text)
+    event.preventDefault();
+  })
 });
